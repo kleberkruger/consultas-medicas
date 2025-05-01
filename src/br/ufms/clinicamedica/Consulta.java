2eba0e8c26f8b8e24ed54c74d73d16f0a132a23d
Kleber Kruger <kleberkruger@gmail.com>
2025-05-01 01:33:09 -0400

    Projeto inicial

--- src/br/ufms/clinicamedica/Consulta.java
+++ src/br/ufms/clinicamedica/Consulta.java
@@ -1,4 +1,25 @@
 package br.ufms.clinicamedica;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Consulta {
+
+    private final long codigo;
+    private final Medico medico;
+    private final Paciente paciente;
+    private String sintomas;
+    private LocalDateTime dataHora;
+    private double valor;
+    private String receita;
+    private List<String> exames;
+
+    public Consulta(Medico medico, Paciente paciente, String sintomas, LocalDateTime dataHora, double valor) {
+        this.codigo = 0;
+        this.medico = medico;
+        this.paciente = paciente;
+        this.sintomas = sintomas;
+        this.dataHora = dataHora;
+        this.valor = valor;
+    }
 }
--- src/br/ufms/clinicamedica/Endereco.java
+++ src/br/ufms/clinicamedica/Endereco.java
@@ -1,4 +1,77 @@
 package br.ufms.clinicamedica;
 
 public class Endereco {
+
+    private String logradouro;
+    private Integer numero;
+    private String complemento;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private Estado estado;
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        if (logradouro == null || logradouro.isEmpty()) {
+            throw new IllegalArgumentException("Logradouro nulo ou vazio");
+        }
+        this.logradouro = logradouro;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        if (numero != null && numero < 0) {
+            throw new IllegalArgumentException("Numero inválido");
+        }
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        if (complemento != null && complemento.length() > 50) {
+            throw new IllegalArgumentException("O complemento deve ter no máximo 50 caracteres");
+        }
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
 }
